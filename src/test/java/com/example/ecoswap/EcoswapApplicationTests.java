a8507635c54ea5519aa526f6fdd4ab63de885d62
sumalsurendra1999@gmail.com <sumalsurendra1999@gmail.com>
2025-10-21 23:19:47 +0530

    Initial setup of the project

---
+++ src/test/java/com/example/ecoswap/EcoswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
